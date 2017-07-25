9ba04101b607e7c54a3893100e9aaf7fa35d68a9
maghassan <elminu80@gmail.com>
2017-07-26 00:19:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aminu/wapaexchangekano/atm_rates.java
@@ -0,0 +1,13 @@
+package com.example.aminu.wapaexchangekano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class atm_rates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atm_rates);
+    }
+}
