ac3577cd8e3ca9d5ae82c21cf3c3d84f0e3432a2
maghassan <elminu80@gmail.com>
2017-08-02 01:05:26 +0100

    3rd commit.

--- app/src/main/java/com/example/aminu/wapaexchangekano/about.java
+++ app/src/main/java/com/example/aminu/wapaexchangekano/about.java
@@ -18,69 +18,10 @@ import com.squareup.picasso.Picasso;
 
 public class about extends AppCompatActivity {
 
-    private RecyclerView mBlogList;
-    FirebaseDatabase database;
-    DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        //Recycler View
-        mBlogList = (RecyclerView)findViewById(R.id.postList);
-        mBlogList.setHasFixedSize(true);
-        mBlogList.setLayoutManager(new LinearLayoutManager(this));
-
-        // Send a Query to the database
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Data");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        FirebaseRecyclerAdapter<ModelClass, BlogViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<ModelClass, BlogViewHolder>(
-                        ModelClass.class,
-                        R.layout.design_row,
-                        BlogViewHolder.class,
-                        myRef)  {
-
-
-
-                    @Override
-                    protected void populateViewHolder(BlogViewHolder viewHolder, ModelClass model,int position) {
-                        viewHolder.setTitle(model.getTitle());
-                        viewHolder.setImage(getApplicationContext(), model.getImage());
-                    }
-                };
-        mBlogList.setAdapter(firebaseRecyclerAdapter);
-    }
-    //View Holder For Recycler View
-    public static class BlogViewHolder extends RecyclerView.ViewHolder  {
-        View mView;
-        public BlogViewHolder(View itemView) {
-            super(itemView);
-            mView= itemView;
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent browserIntent = new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("http://www.androidsquad.space/"));
-                    Intent browserChooserIntent = Intent.createChooser(browserIntent , "Choose browser of your choice");
-                    v.getContext().startActivity(browserChooserIntent);
-                }
-            });
-        }
-        public void setTitle(String title){
-            TextView post_title = (TextView)mView.findViewById(R.id.titleText);
-            post_title.setText(title);
-        }
-        public void setImage(Context ctx , String image){
-            ImageView post_image = (ImageView)mView.findViewById(R.id.imageView);
-            // We Need TO pass Context
-            Picasso.with(ctx).load(image).into(post_image);
-        }
     }
 }
--- app/src/main/java/com/example/aminu/wapaexchangekano/home.java
+++ app/src/main/java/com/example/aminu/wapaexchangekano/home.java
@@ -7,92 +7,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class home extends Fragment {
 
-    TextView usbp1t1;
-    TextView ussp1t1;
-    TextView ukbp1t1;
-    TextView uksp1t1;
-    TextView eubp1t1;
-    TextView eusp1t1;
-    TextView cnbp1t1;
-    TextView cnsp1t1;
+    TextView usbt1;
+    TextView usst1;
+    TextView ukbt1;
+    TextView ukst1;
+    TextView eubt1;
+    TextView eust1;
+    TextView cnbt1;
+    TextView cnst1;
 
-    TextView usbp1t2;
-    TextView ussp1t2;
-    TextView ukbp1t2;
-    TextView uksp1t2;
-    TextView eubp1t2;
-    TextView eusp1t2;
-    TextView cnbp1t2;
-    TextView cnsp1t2;
+    TextView usbt2;
+    TextView usst2;
+    TextView ukbt2;
+    TextView ukst2;
+    TextView eubt2;
+    TextView eust2;
+    TextView cnbt2;
+    TextView cnst2;
 
-    TextView usbp1t3;
-    TextView ussp1t3;
-    TextView ukbp1t3;
-    TextView uksp1t3;
-    TextView eubp1t3;
-    TextView eusp1t3;
-    TextView cnbp1t3;
-    TextView cnsp1t3;
-
-    FirebaseDatabase database;
-    DatabaseReference ChangeRef;
+    TextView usbt3;
+    TextView usst3;
+    TextView ukbt3;
+    TextView ukst3;
+    TextView eubt3;
+    TextView eust3;
+    TextView cnbt3;
+    TextView cnst3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.activity_home, container, false);
 
-        database = FirebaseDatabase.getInstance();
-        ChangeRef = database.getReference("ChangeRef");
-
-        usbp1t1 = (TextView) rootView.findViewById(R.id.usbp1t1);
-        ussp1t1 = (TextView) rootView.findViewById(R.id.ussp1t1);
+        usbt1 = (TextView) rootView.findViewById(R.id.usbt1);
+        usst1 = (TextView) rootView.findViewById(R.id.usst1);
 
         return rootView;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        ChangeRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String text = dataSnapshot.getValue(String.class);
-                usbp1t1.setText(text);
-
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                String text = dataSnapshot.getValue(String.class);
-                usbp1t1.setText(text);
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
     }
 }
