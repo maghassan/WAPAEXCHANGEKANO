2f1fc0bcefcb71e92f454894ad029fdad0f18b1e
maghassan <elminu80@gmail.com>
2017-08-14 13:23:59 +0100

    6th commit.

--- app/src/main/java/com/example/aminu/wapaexchangekano/home.java
+++ app/src/main/java/com/example/aminu/wapaexchangekano/home.java
@@ -467,5 +467,4 @@ public class home extends Fragment {
             }
         });
     }
-
 }
--- app/src/main/java/com/example/aminu/wapaexchangekano/weekly.java
+++ app/src/main/java/com/example/aminu/wapaexchangekano/weekly.java
@@ -1,13 +1,28 @@
 package com.example.aminu.wapaexchangekano;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class weekly extends AppCompatActivity {
+
+    private Button next;
+    private Button pre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly);
+
+        next = (Button) findViewById(R.id.next);
+        pre = (Button) findViewById(R.id.pre);
     }
+
+    public void nextday(View view){
+        Intent a = new Intent(this, Tuesday.class);
+        startActivity(a);
+    }
+
 }
